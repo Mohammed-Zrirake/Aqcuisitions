1dd8950e107ae74bc7a3ceafe4cc11efb5b18377
mohammed.Zr <mohammed.zrirake01@gmail.com>
2025-11-21 09:37:58 +0100

    add project dependencies

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-const PORT = 3000 || process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
