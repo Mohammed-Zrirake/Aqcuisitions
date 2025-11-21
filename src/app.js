b0cafcbece72cc4490e6d78602bd4c74711acc8a
mohammed.Zr <mohammed.zrirake01@gmail.com>
2025-11-21 11:46:56 +0100

    added Security Using Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
 app.use(helmet());
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
-
+app.use(securityMiddleware);
 app.get('/', (req, res) => {
   logger.info('Hello World! from Aquistions API');
   res.send('Hello World!');
